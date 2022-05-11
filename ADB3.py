7bba9b4e0d7166c93b8117a8b12589d42797887d
AndreBastosB <andrebastos31@gmail.com>
2022-05-11 18:05:27 -0300

    -1 DB

--- ADB3.py
+++ ADB3.py
@@ -12,6 +12,7 @@ screen_height = root.winfo_screenheight()
 x = (screen_width/2) - (width/2)
 y = (screen_height/2) - (height/2)
 root.geometry("%dx%d+%d+%d" % (width, height, x, y))
+
 root.resizable(0, 0)
 
 #========================================INITIALIZATION===================================
